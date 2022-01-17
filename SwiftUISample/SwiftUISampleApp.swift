9214552667e9a54bf7b7c84485346e4c699b3b0d
zafyraminx <zafra_angelo_26@yahoo.com.ph>
2022-01-17 09:26:14 +0800

    Initial Commit

---
+++ SwiftUISample/SwiftUISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISampleApp.swift
+//  SwiftUISample
+//
+//  Created by Michael Angelo Zafra on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
